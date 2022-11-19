7e624b75acb039a116d2cd7b53200824374683e0
Maruf Hasan <118299538+marufbuet@users.noreply.github.com>
2022-11-19 23:07:12 +0600

    apkCombinerFinal
    November, 2022

---
+++ ApkCombiner/release/version-0.1.0/runApkCombiner.sh
@@ -0,0 +1,13 @@
+cp=.
+cp=$cp:ApkCombiner_lib/commons-io-2.4.jar
+cp=$cp:ApkCombiner_lib/jdom-2.0.5.jar
+cp=$cp:ApkCombiner_lib/apktool.jar
+cp=$cp:ApkCombiner.jar
+
+rm -rf workspace
+mkdir workspace
+
+java -cp $cp iccta.apkcombiner.Main $*
+
+rm -rf works
+pace
